8fd94de30ce42111acecf7abcb2ee9d7c0c55fcd
nachi <scarl.natsu729@gmail.com>
2025-02-04 01:15:22 +0900

    マッピングを用いた処理の効率化

--- move.go
+++ move.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"flag"
-	"fmt"
-	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
@@ -11,88 +9,78 @@ import (
 )
 
 func main() {
+	// コマンドライン引数を明示
+	sourceDirPtr := flag.String("source", "", "移動元ディレクトリのパス")
+	targetDirPtr := flag.String("target", "", "移動先ディレクトリのパス")
+
+	// 入力をパース
 	flag.Parse()
 
-	// 移動元ディレクトリのパスを引数から取得
-	source_dir := flag.Arg(0)
+	// 引数が正しくない場合は実行方法を明示
+	if *sourceDirPtr == "" || *targetDirPtr == "" {
+		log.Fatal("使用方法: go run move.go -source=<移動元ディレクトリ> -target=<移動先ディレクトリ>")
+	}
 
-	// 移動先ディレクトリのパスを引数から取得
-	target_dir := flag.Arg(1)
+	sourceDir := *sourceDirPtr
+	targetDir := *targetDirPtr
 
 	// 移動元ディレクトリの中の全てのファイル情報を取得
-	source_files, err := getFiles(source_dir)
+	sourceFiles, err := getFiles(sourceDir)
 
 	if err != nil {
 		log.Fatal(err)
-		panic(err)
 	}
 
-	// 拡張子が".JPG"のファイル名を取得する
-	source_file_names := getSourceFileNames(source_files)
+	// JPG → RAF のマッピングを作成
+	targetFileMap := createTargetFileMap(sourceFiles)
 
-	// 取得したJPEGファイルのファイル名を一致判定用に".RAF"に変換する
-	target_file_names := getTargetFileNames(source_file_names)
+	// RAF ファイルを移動
+	moveFiles(sourceFiles, targetFileMap, sourceDir, targetDir)
 
-	// 指定したファイルを移動元ディレクトリから移動先ディレクトリへ移動
-	moveFiles(source_files, target_file_names, source_dir, target_dir)
-
-	fmt.Println("move completed!")
+	log.Println("move completed!")
 }
 
-func getFiles(source_dir string) ([]os.FileInfo, error) {
-	// 移動元ディレクトリの中のファイル全てを取得、格納
-	source_files, err := ioutil.ReadDir(source_dir)
+// used by main()
+func getFiles(source_dir string) ([]os.DirEntry, error) {
+	// 対象ディレクトリの中のファイル全てを取得、格納
+	files, err := os.ReadDir(source_dir)
+
 	// エラーがあれば"err"を返す
 	if err != nil {
 		return nil, err
 	}
 
-	return source_files, nil
+	return files, nil
 }
 
-func getSourceFileNames(files []os.FileInfo) []string {
-	var source_file_names []string
+// used by main()
+func createTargetFileMap(files []os.DirEntry) map[string]struct{} {
+	targetFileMap := make(map[string]struct{})
 	for _, file := range files {
 		// JPEGファイルだけを選別して、ファイル名のみをスライスに格納
-		file_extention := filepath.Ext(file.Name())
-		if file_extention == ".JPG" || file_extention == ".jpg" || file_extention == ".jpeg" {
-			source_file_names = append(source_file_names, file.Name())
+		ext := strings.ToLower(filepath.Ext(file.Name()))
+		if ext == ".jpg" || ext == ".jpeg" {
+			raw_file := strings.TrimSuffix(file.Name(), ext) + ".RAF"
+			targetFileMap[raw_file] = struct{}{}
 		}
 	}
-	// 格納したファイル名のスライスを返す
-	return source_file_names
+	return targetFileMap
 }
 
-func getTargetFileNames(file_names []string) []string {
-	var target_file_names []string
-	for _, file_name := range file_names {
-		// 拡張子を".JPG"から".RAF"に変換してスライスに格納
-		switch filepath.Ext(file_name) {
-		case ".JPG":
-			target_file_names = append(target_file_names, strings.Replace(file_name, ".JPG", ".RAF", -1))
-		case ".jpg":
-			target_file_names = append(target_file_names, strings.Replace(file_name, ".jpg", ".RAF", -1))
-		case ".jpeg":
-			target_file_names = append(target_file_names, strings.Replace(file_name, ".jpeg", ".RAF", -1))
-		}
-	}
-	// 格納したファイル名のスライスを返す
-	return target_file_names
-}
-
-func moveFiles(files []os.FileInfo, target_file_names []string, source_dir string, target_dir string) {
+// used by main()
+func moveFiles(files []os.DirEntry, targetFileMap map[string]struct{}, sourceDir, targetDir string) {
 	for _, file := range files {
-		for _, target_file_name := range target_file_names {
-			// 移動用の疑似的なファイル名と一致するファイルを選別
-			if filepath.Base(file.Name()) == target_file_name {
-				// ファイル移動
-				err := os.Rename(source_dir+"/"+file.Name(), target_dir+"/"+file.Name())
-				// エラーがあればその場で出力
-				if err != nil {
-					fmt.Println(err)
-				}
+		// ファイル名を用いて移行対象か判定して対象なら移行を実行
+		if _, exists := targetFileMap[file.Name()]; exists {
+			sourcePath := filepath.Join(sourceDir, file.Name())
+			targetPath := filepath.Join(targetDir, file.Name())
 
+			if err := os.Rename(sourcePath, targetPath); err != nil {
+				log.Printf("Failed %s: %v", file.Name(), err)
+			} else {
+				log.Printf("Moved: %s → %s", file.Name(), targetPath)
 			}
 		}
+
 	}
 }
